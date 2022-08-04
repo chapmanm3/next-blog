2e64f9d3b2f0af6d3d56158cc62256b312ba0d86
Matt Chapman <chapmancac@gmail.com>
2022-08-04 15:58:06 -0400

    Get contact form working

--- pages/api/contactForm.ts
+++ pages/api/contactForm.ts
@@ -1,9 +1,28 @@
+const sgMail = require('@sendgrid/mail')
+
+function sendMail(message: string) {
+  sgMail.setApiKey(process.env.SENDGRID_API_KEY)
+  
+  const msg = {
+    to: 'chapmancac@gmail.com',
+    from: 'contact@mattchapman.tech',
+    subject: 'Youve got a new Contact',
+    text: message
+  }
+
+  sgMail.send(msg)
+    .then(() => console.log('Contact sent successfully'))
+    .catch((error) => console.error(error))
+}
+
 export default function handler(req, res) {
 
   const body = req.body;
 
   console.log("Form Data: ");
   console.log(body);
+
+  sendMail(`Email: ${body.email} Message: ${body.message}`)
 
   return res.status(200).json({data: "looks good boi"});
 
