707d5282647f1894ba75fa2323b82089ee8f0966
Matt Chapman <chapmancac@gmail.com>
2026-04-23 12:15:12 -0400

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google"
 import React from "react";
 import { NavBar } from "../components/NavBar/navbar";
 import { Analytics } from '@vercel/analytics/next'
+import { Metadata } from "next";
 
 import '../styles/globals.css'
 
@@ -10,6 +11,36 @@ const inter = Inter({
   display: "swap",
   variable: '--font-inter',
 })
+
+export const metadata: Metadata = {
+  metadataBase: new URL(process.env.SITE_URL || 'http://localhost:3000'),
+  title: {
+    default: "Matt Chapman",
+    template: "%s | Matt Chapman"
+  },
+  description: "The personal site for Matt Chapman, a web developer from Boston, MA.",
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: process.env.SITE_URL,
+    siteName: 'Matt Chapman',
+    title: 'Matt Chapman',
+    description: "The personal site for Matt Chapman, a web developer from Boston, MA.",
+    images: [
+      {
+        url: '/og-image.png',
+        width: 1200,
+        height: 630,
+      }
+    ]
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Matt Chapman',
+    description: "The personal site for Matt Chapman, a web developer from Boston, MA.",
+    images: ['/og-image.png']
+  }
+}
 
 interface RootLayoutProps {
   children: React.ReactNode
