34ea39fd60c00f4b17f8439be93ce3a986b20651
Matt Chapman <chapmancac@gmail.com>
2026-04-23 11:26:45 -0500

    Mobile blog styling (#11)
    * styles
    
    * metadata

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,12 +1,24 @@
 import styles from './BlogPost.module.css'
 import BackButton from "../../../components/BackButton"
 import { blogEntries } from '../blogEntries';
+import { Metadata } from "next";
 
 export async function generateStaticParams() {
   return blogEntries
 }
 
 export const dynamicParams = false;
+
+export async function generateMetadata({ params }: { params: Promise<{ slug: string }> }): Promise<Metadata> {
+  const { slug } = await params
+  const post = blogEntries.find(p => p.slug === slug)
+  const title = post?.title || "Blog Post"
+  
+  return {
+    title: `${title} | Matt Chapman`,
+    description: `Read about ${title.toLowerCase()} on Matt Chapman's blog.`,
+  }
+}
 
 export default async function BlogPostPage({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params
